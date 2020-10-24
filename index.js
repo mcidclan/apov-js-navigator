b4da537ca1dce8ffd96d6314ebd267310c056d21
mcidclan <m.cid.clan@gmail.com>
2020-10-24 16:08:44 +0200

    -Reviews server to serve frames.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require("express")();
+const express = require('express');
+const fs = require('fs');
 
-app.get('/hello', (req, res) => {
-    return res.send("Hello from APoV server!");
+const app = express();
+
+app.get('/frame/:offset/:size', (req, res) => {
+    const offset = +req.params.offset;
+    const size = Math.pow(+req.params.size, 2)*4;
+    
+    const stream = fs.createReadStream(__dirname + '/atoms-done.bin', {
+        highWaterMark: size,        
+        encoding: 'binary',
+        start: offset
+    });
+    
+    stream.on("data", (chunk) => {
+        stream.destroy();
+        res.end(chunk, 'binary');
+    }); 
 });
 
 app.listen(5000);

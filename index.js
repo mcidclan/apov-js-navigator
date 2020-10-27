26749c9845a6a279ae1144eb3f91acf9ebe026db
mcidclan <m.cid.clan@gmail.com>
2020-10-27 10:35:00 +0100

    -Adds the /header route and implements the endpoint accordingly.

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const HEADER_SIZE = 20*4;
+
+app.get('/header', (req, res) => {
+    fs.open(__dirname + '/atoms.apov', 'r', (err, fd) => {
+        if(!err) {
+            const buffer = Buffer.allocUnsafe(HEADER_SIZE);
+            fs.read(fd, buffer, 0, HEADER_SIZE, 0, (err, count, header) => {
+                if(!err && count == HEADER_SIZE) {
+                    res.end(header, 'binary');
+                } else res.status(500).end();
+            });
+        } else res.status(500).end();
+    });
+});
 
 app.get('/frame/:offset/:size', (req, res) => {
-    const offset = +req.params.offset;
+    const offset = +req.params.offset + HEADER_SIZE;
     const size = Math.pow(+req.params.size, 2)*4;
-    const stream = fs.createReadStream(__dirname + '/atoms-done.bin', {
-        highWaterMark: size,        
+    const stream = fs.createReadStream(__dirname + '/atoms.apov', {
+        highWaterMark: size,
         encoding: 'binary',
         start: offset
-    });
-    
-    stream.on("data", (chunk) => {
+    }).on('data', (chunk) => {
         stream.destroy();
         res.end(chunk, 'binary');
-    }); 
+    }).on('erro', () => {
+        res.status(500).end();
+    });
 });
 
 app.listen(5000);
